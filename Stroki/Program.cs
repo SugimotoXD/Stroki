ceb0159f48693680008bca3ce78086b3a869f584
fsafdsa Lfybkf <P.danila228@mail.ru>
2020-12-18 04:01:59 +0200

    Добавьте файлы проекта.

---
+++ Stroki/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stroki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "He is good!";
+
+            s1 = s1.Remove(0, 2);
+
+            Console.WriteLine(s1);
+
+            Console.ReadLine();
+        }
+    }
+}
